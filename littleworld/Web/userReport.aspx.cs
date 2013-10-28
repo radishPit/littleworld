6f44cba1a185859a20ca819ff1958cf65213928f
cslghuzhijia <1037737453@qq.com>
2013-10-28 10:07:49 +0800

    change
    change

--- littleworld/Web/userReport.aspx.cs
+++ littleworld/Web/userReport.aspx.cs
@@ -9,26 +9,9 @@ namespace littleworld.Web
 {
     public partial class userReport : System.Web.UI.Page
     {
-        public int noveltyID = 61;
-        public string contents = "";
-        public int senderID;
-        public string userName = "";
-        public string headImgUrl = "";
         protected void Page_Load(object sender, EventArgs e)
         {
-             //noveltyID = Request.QueryString.Get("noveltyID");
-            getuserinfo();
-        }
-        public void getuserinfo()
-        {
-            BLL.noveltyTb bll = new BLL.noveltyTb();
-            Model.noveltyTb ml = bll.GetModel(noveltyID);
-            contents = ml.contents;
-            senderID = ml.senderID;
-            BLL.userTb bllu = new BLL.userTb();
-            Model.userTb mlu = bllu.GetModel(senderID);
-            userName=mlu.userName;
-            headImgUrl = mlu.headImgUrl;
+
         }
     }
 }
